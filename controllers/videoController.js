d4d642f37ab46283d467bb96880639bf5fd934f9
kooo8282 <kooo8282@gmail.com>
2021-01-17 23:04:38 +0900

    2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,12 @@
 export const home = (req, res) => res.render("home", {pageTitle:"Home"});
-export const search = (req, res) => res.render("search", {pageTitle:"Search"});
+
+export const search = (req, res) => {
+    // const searchingBy = req.query.term;
+    // res.render("search", {pageTitle:"Search", searchingBy:searchingBy});
+    const {query: { term: searchingBy }} = req;
+    res.render("search", {pageTitle:"Search", searchingBy});
+}
+
 export const video = (req, res) => res.render("video", {pageTitle:"Video"});
 export const upload = (req, res) => res.render("upload", {pageTitle:"Upload"});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"Video Detail"});
