67d3c2682b4a9809947424a8841bdda336d0c4fe
kooo8282 <70710735+kooo8282@users.noreply.github.com>
2021-01-18 19:50:56 +0900

    2.20 Change Profile HTML

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,9 +4,9 @@ import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
+// userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
