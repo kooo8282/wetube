b8374019a1d185250ee97376ac1398eb93849bbe
kooo8282 <70710735+kooo8282@users.noreply.github.com>
2021-01-18 20:42:00 +0900

    2.21 Home Controller(make Fake DB)

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,14 +1,19 @@
-export const home = (req, res) => res.render("home", {pageTitle:"Home"});
+import { videos } from "../db"
+
+export const home = (req, res) => {
+    res.render("home", { pageTitle: "Home", videos }); //now home template has videos Obj.
+
+}
 
 export const search = (req, res) => {
     // const searchingBy = req.query.term;
     // res.render("search", {pageTitle:"Search", searchingBy:searchingBy});
-    const {query: { term: searchingBy }} = req;
-    res.render("search", {pageTitle:"Search", searchingBy});
+    const { query: { term: searchingBy } } = req;
+    res.render("search", { pageTitle: "Search", searchingBy });
 }
 
-export const video = (req, res) => res.render("video", {pageTitle:"Video"});
-export const upload = (req, res) => res.render("upload", {pageTitle:"Upload"});
-export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"Video Detail"});
-export const editVideo = (req, res) => res.render("editVideo", {pageTitle:"Edit Video"});
-export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle:"Delete Video"});
+export const video = (req, res) => res.render("video", { pageTitle: "Video" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 232325,
+        title: "Video awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+        }
+    },
+    {
+        id: 234354,
+        title: "Video super",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+        }
+    },
+    {
+        id: 234565,
+        title: "Video nice",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+        }
+    },
+    {
+        id: 786556,
+        title: "Video perfect",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+        }
+    }
+];
