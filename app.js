e35ebcf9fc1f3a29459a846ce92d9e1e9cb9f1dc
kooo8282 <70710735+kooo8282@users.noreply.github.com>
2021-01-19 12:34:09 +0900

    Home Controller part Two(mixin like func.)

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(function (req, res, next) {
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
+    next();
+});
+
 
 app.use(localsMiddleware)
 
