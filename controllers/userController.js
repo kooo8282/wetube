a2c1934a1d7095acd396dc89f827abef90b18533
kooo8282 <70710735+kooo8282@users.noreply.github.com>
2021-01-20 20:10:10 +0900

    2.24 Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+import routes from "../routes";
+
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
 };
@@ -18,7 +20,7 @@ export const postJoin = (req, res) => {
 }
 
 export const getLogin = (req, res) =>
-    res.render("login", { pageTitle: "Login" });
+    res.render("login", { pageTitle: "Log In" });
 export const postLogin = (req, res) => {
     res.redirect(routes.home);
 };
