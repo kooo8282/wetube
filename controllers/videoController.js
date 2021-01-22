fee8d6a7e511283595e5d4ad7cc22f01253ac992
kooo8282 <kooo8282@gmail.com>
2021-01-23 00:50:48 +0900

    2.25 More Controlers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db"
+import routes from "../routes"
 
 export const home = (req, res) => {
     res.render("home", { pageTitle: "Home", videos }); //now home template has videos Obj.
@@ -13,7 +14,19 @@ export const search = (req, res) => {
 }
 
 export const video = (req, res) => res.render("video", { pageTitle: "Video" });
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+
+export const getUpload = (req, res) => {
+    res.render("upload", { pageTitle: "Upload" });
+}
+
+export const postUpload = (req, res) => {
+    const {
+        body:{file, title, description}
+        } = req;
+    // To do : Upload and save video
+    res.redirect(routes.videoDetail(232325)) //fake id in fake db
+};
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
