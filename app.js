368079309ee6a574707c308ad940f1f7318cb2b4
kooo8282 <70710735+kooo8282@users.noreply.github.com>
2021-02-05 20:22:46 +0900

    3.11 Installing ESLint

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
     next();
 });
