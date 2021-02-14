c8d4de88685b2b829ab34253483a696a6ed1191f
kooo8282 <kooo8282@gmail.com>
2021-02-15 00:25:23 +0900

    5.1 Footer, Login & Join & Ignore 'scss'

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTue";
     res.locals.routes = routes;
     res.locals.user = { //fake user information
-        isAuthenticated: true,
+        isAuthenticated: false,
         id: 1
     }
     next();
