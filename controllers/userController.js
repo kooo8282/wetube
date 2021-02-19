178adced2b3590d4ec6fc8c898801f1d10ba999b
kooo8282 <kooo8282@gmail.com>
2021-02-20 02:12:33 +0900

    6.3 Loggin the User In

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
+import passport from "passport";
 import routes from "../routes";
+import User from "../models/User"
 
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res, next) => {
     // console.log(req.body);
     const { //ES6 method
         body: { name, email, password, password2 }
@@ -14,16 +16,28 @@ export const postJoin = (req, res) => {
         res.render("join", { pageTitle: "Join" });
     } else {
         //To Do: Regisger User
+        try {
+            const user = await User.create({
+                name,
+                email
+            })
+            await User.register(user, password);
+            next();
+        } catch(error){
+            console.log(error);
+            res.redirect(routes.home);
+        }
         //To Do: Log user in
-        res.redirect(routes.home);
     }
 }
 
 export const getLogin = (req, res) =>
     res.render("login", { pageTitle: "Log In" });
-export const postLogin = (req, res) => {
-    res.redirect(routes.home);
-};
+
+export const postLogin = passport.authenticate('local', {
+    failureRedirect: routes.login,
+    successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
     // Todo : Process Log out
--- passport.js
+++ passport.js
@@ -2,3 +2,7 @@ import passport from "passport";
 import User from "./models/User";
 
 passport.use(User.createStrategy()); // shortcut for strategy
+
+// shortcut by passport-lacal-mongoose
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser());
